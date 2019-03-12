81b6ce50103f27efd23dedc10b0b014573ef3ec6
Баричев Юрий <bar2104y@yandex.ru>
2019-03-12 18:26:27 +0300

    Добавлен парсинг истории блокировок, смены имен, обращений в поддержку, списка друзей. Унифицирована функция парсинга блолка

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import re
+from pprint import pprint
+
+def skipLines():
+	line = ''
+	inputFile.readline()
+	while line != '---\n':
+		line = inputFile.readline()
 
 #Разовый поиск в строке
 def searchOnce(string):
@@ -10,16 +17,63 @@ def searchOnce(string):
 			res = tmp[0][1]
 			return res
 
-def parseLoginHistory():
+def parseBlock():
 	line = ''
-	LHArr = []
+	Arr = []
 	while line != '---':
 		line = inputFile.readline()
 		if line != '\n':
 			line = line.replace('\n', '')
-			LHArr.append(line)
-	del LHArr[len(LHArr)-1]
-	return LHArr
+			Arr.append(line)
+	del Arr[len(Arr)-1]
+	return Arr
+
+#Получение списка друзей
+# Return [[link, name],
+#		  [link, name]]
+def parseFriends():
+	line = ''
+	Arr = []
+	inputFile.readline()
+	while line != '---':
+		line = inputFile.readline()
+		if line != '\n':
+			line = line.replace('\n', '')
+			tmp = line.split(' ')
+			#tmp[1].split('(')
+			#tmp[2].split(')')
+			Arr.append(tmp)
+	del Arr[len(Arr)-1]
+	for t in Arr:
+		print(t)
+		t[1] = t[1].replace('(', '')
+		t[2] = t[2].replace(')', '')
+
+	del Arr[len(Arr)-1]
+	return Arr
+
+
+# def parseLoginHistory()link, name:
+# 	line = ''
+# 	LHArr = []
+# 	while line != '---':
+# 		line = inputFile.readline()
+# 		if line != '\n':
+# 			line = line.replace('\n', '')
+# 			LHArr.append(line)
+# 	del LHArr[len(LHArr)-1]
+# 	return LHArr
+
+# def parseNameHistory():
+# 	line = ''
+# 	LHArr = []
+# 	while line != '---':
+# 		line = inputFile.readline()
+# 		if line != '\n':
+# 			line = line.replace('\n', '')
+# 			LHArr.append(line)
+# 	del LHArr[len(LHArr)-1]
+# 	return LHArr
 
 
 path = 'log.txt'
@@ -45,13 +99,34 @@ try:
 	lastIP = searchOnce('(IP-адрес последнего посещения: )(.*)')
 	phone = searchOnce('(Телефон: )(.*)')
 
-	print(email)
-	print(regDate)
-	print(lastLogin)
-	print(lastIP)
+	#print(email)
+	#print(regDate)
+	#print(lastLogin)
+	#print(lastIP)
 
 	inputFile.readline()
-	print(parseLoginHistory())
+	LoginHistory = parseBlock()
+	print(LoginHistory)
+
+	skipLines()
+	skipLines()
+
+
+	NameHistory = parseBlock()
+	print(NameHistory)
+
+	blockHistory = parseBlock()
+	print(blockHistory)
+
+	supportHistory = parseBlock()
+	print(supportHistory)
+
+	recoveryHistory = parseBlock()
+	print(recoveryHistory)
+
+	print(parseFriends())
+
+
 	
 
 
