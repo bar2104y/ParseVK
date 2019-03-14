436f0442b0a2df90fd13cb0f660ebd53b223a8cb
Баричев Юрий <bar2104y@yandex.ru>
2019-03-14 11:09:23 +0300

    Временно

--- messages.py
+++ messages.py
@@ -26,15 +26,48 @@ def parseDialogs(name, arr):
 #     </body>
 # </html>
 # '''
-	for message in arr:
-		print(message)
-		# Разбор информации о сообщении в переменные
-		direction = message[0]
-		id = re.search(r'(vk.com\/id)(.*)', message[1])
-		id = id.group(2)
-		tmp = str(message[2]).split(' в ')
-		date = tmp[0]
-		time = tmp[1]
-		message = message[3]
+	#диалоги
+	dialogListAdr = []
 
-		# перевод данных в json
+	# Составление списка диалогов
+	for message in arr:
+		direction = message[0]
+		if len(message)>1:
+			id = re.search(r'(vk.com\/id)(.*)', message[1])
+			id = id.group(2)
+
+			f = False
+			for i in range(0, len(dialogListAdr)):
+				if dialogListAdr[i] == id:
+					f = True
+					break
+			if not f:
+				dialogListAdr.append(id)
+
+	dialogListData = []
+	
+	for i in range(0, len(dialogListAdr)):
+		dialogListData.append([])
+	
+		
+
+	for message in arr:
+		direction = message[0]
+		if len(message)>1:
+			id = re.search(r'(vk.com\/id)(.*)', message[1])
+			id = id.group(2)
+			tmp = str(message[2]).split(' в ')
+			date = tmp[0]
+			time = tmp[1]
+			message = message[3]
+
+		#перекомпановка данн
+		
+
+
+		#messageData = [direction,date,time,message]
+
+		#dialogListData[dialogID].append(messageData)
+
+	return
+		
