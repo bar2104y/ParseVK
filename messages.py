6b167ab4a67dc0c27a781826c2789797389f6e0d
Баричев Юрий <bar2104y@yandex.ru>
2019-06-19 21:10:25 +0300

    Исправлено большое количество ошибок. Из вложений работают только фото

--- messages.py
+++ messages.py
@@ -1,60 +1,90 @@
 import re
+from attachments import getPhoto
 
+def attachmentsType(inputtext):
+	#photo audio wall video sticker doc gift link
+	at = re.search(r'([0-9]*)( )(.*?)( )(.*\))', inputtext)
+	at = at.group(3) if at != None else 'Error'
+	return(at)
+
+
+# форматирование вложения
 def parseAttachments(mesAttachments):
-	string = ''
-	for a in mesAttachments:
-		string += str(a)
-	atInfo = re.search(r'(Прикрепление #)([0-9]*)( )(.*)?( )(.*)', string)
-	print(atInfo)
-	print(string)
-	print()
+	string = mesAttachments
+
+	aType = attachmentsType(string)
+	
+	#Костыль вместо switch case
+	if aType == 'photo':
+		link = getPhoto(string)
+		tmp = '<a src="' + link + '"><img src = "' + link + '"></a>'
+	else:
+		tmp = 'Вложение ' + aType + '<br>'
+	return(tmp)
+	
 
 
 def parseDialogs(name, arr):
 	#диалоги
+	#arr - все сообщения(вообще все)
 	dialogListAdr = []
+	print('Разбор диалогов')
 
 	# Составление списка диалогов
 	for message in arr:
 		# Костыль для отсеивания сообщений из чатов(не личных)
-		if len(message)>1:
+		if len(message) >= 3:
 			# Получение id собеседника
 			id = re.search(r'(vk.com\/id)(.*)', message[1])
-			id = id.group(2)
+			if id != None:
+				id = id.group(2)
 
-			# определяет уникальность собеседника
-			# Если встречается первый раз - записываем, иначе - игнорируем
-			f = False
-			for i in range(0, len(dialogListAdr)):
-				if dialogListAdr[i] == id:
-					f = True
-					break
-			if not f:
-				dialogListAdr.append(id)
+				# определяет уникальность собеседника
+				# Если встречается первый раз - записываем, иначе - игнорируем
+				f = False
+				for i in range(0, len(dialogListAdr)):
+					if dialogListAdr[i] == id:
+						f = True
+						break
+				if not f:
+					dialogListAdr.append(id)
 	
 	# Подготовка массива с сообщениями
 	# Создание структуры массива
 	dialogListData = []
-	for i in range(0, len(dialogListAdr)):
+	for i in range(len(dialogListAdr)):
 		dialogListData.append([])
 
+	#перебор сообщений, распределение по диалогам
+	'''
+	message : [1, 'https://vk.com/id12565', '13.10.2015 в 17:49:21', 'не хочу об этом говорить']
+
+	'''
 	for message in arr:
-		direction = message[0]
-		# Костыль, отсеивающий сообщения из чатов
-		# Надеюсь исправить потом
-		if len(message)>1:
+		
+		if len(message) >= 3:
+			direction = message[0]
 			id = re.search(r'(vk.com\/id)(.*)', message[1])
 			id = id.group(2)
 			tmp = str(message[2]).split(' в ')
 			date = tmp[0]
 			time = tmp[1]
 			message = message[3]
+			message = re.sub(r'Кому:', ' ', message)
+			message = re.sub(r'От кого:', ' ', message)
+
+			attachments = re.findall(r'(Прикрепление #.*)', message)
 			
-			attachments = re.findall(r'(Прикрепление #)(.*\))',message)
 			if len(attachments) > 0:
 				for at in attachments:
-					parseAttachments(at)
-				#print(attachments)
+					at = re.split(r'(Прикрепление #)', at)
+					for a in at:
+						if a != 'Прикрепление #' and a != '':
+							message += '<br> ' + parseAttachments(a)
+			message = re.sub(r'(Прикрепление #[0-9]*)( )(.*?)( )(.*\))', ' ', message)
+			print(date,time, message)
+					
+			
 
 
 			for i in range(0, len(dialogListAdr)):
